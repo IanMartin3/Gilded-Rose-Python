c957eb4b041ff2d8983dea1aa1fe12c3fcade38f
Ian Martin <ian.martin3@capita.com>
2020-09-10 11:37:45 +0100

    added check_item_quality function

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -3,6 +3,10 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def check_item_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
@@ -10,15 +14,12 @@ class GildedRose(object):
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                self.check_item_quality(item)
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        self.check_item_quality(item)
+                    if item.sell_in < 6:
+                        self.check_item_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -30,5 +31,4 @@ class GildedRose(object):
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.check_item_quality(item)
